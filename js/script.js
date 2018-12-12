20fe7167c584b7706d35ec441eae5e17ed730586
Tony Huang <tonyhuang4326@gmail.com>
2018-12-12 02:00:00 +0000

    added lists and arrays

--- js/script.js
+++ js/script.js
@@ -2,15 +2,29 @@
 
 // BELOW Update the songs to your favorites songs. There should be at least 4.
 var songs = [
-    "24 Karat Magic",
-    "Nice For What",
-    "Universe"
+    "Despacito",
+    "Gangnam Style",
+    "Friday",
+    "Pokemon Go",
+    "Pen-Pinapple-Apple-Pen",
+    "Mine Diamonds"
 ];
 // BELOW Add 4 More arrays to store images_links, atists, song lengths, and links for each song
 // Make sure they match the same order as your array above
-
-
-
+var images_urls = [
+    "",
+    "",
+    "",
+    "",
+    "","","",
+    "","",
+    "",
+    "",
+    "","","","",
+];
+var artists = [];
+var songLength = [];
+var links = [];
 
 
 
