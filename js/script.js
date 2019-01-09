09c60a883eb566196235e73c2e56a3bc0e28cec9
Tony Huang <tonyhuang4326@gmail.com>
2019-01-09 02:06:45 +0000

    displayed the items in the correct columns

--- js/script.js
+++ js/script.js
@@ -6,25 +6,39 @@ var songs = [
     "Gangnam Style",
     "Friday",
     "Pokemon Go",
-    "Pen-Pinapple-Apple-Pen",
-    "Mine Diamonds"
+    "Pen-Pinapple-Apple-Pen"
 ];
 // BELOW Add 4 More arrays to store images_links, atists, song lengths, and links for each song
 // Make sure they match the same order as your array above
 var images_urls = [
-    "",
-    "",
-    "",
-    "",
-    "","","",
-    "","",
-    "",
-    "",
-    "","","","",
+    "https://upload.wikimedia.org/wikipedia/en/thumb/c/c8/Luis_Fonsi_Feat._Daddy_Yankee_-_Despacito_%28Official_Single_Cover%29.png/220px-Luis_Fonsi_Feat._Daddy_Yankee_-_Despacito_%28Official_Single_Cover%29.png",
+    "https://upload.wikimedia.org/wikipedia/en/thumb/a/ad/Gangnam_Style_Official_Cover.png/220px-Gangnam_Style_Official_Cover.png",
+    "https://www.nydailynews.com/resizer/jDO546ilsLSbvFlrAtqwf6ZDRCg=/1400x0/arc-anglerfish-arc2-prod-tronc.s3.amazonaws.com/public/LD3YGZSLV6MVXK6GZUCFJTP2YU.jpg",
+    "https://i.ytimg.com/vi/vfc42Pb5RA8/maxresdefault.jpg",
+    "https://www.kiss925.com/wp-content/uploads/sites/59/2016/09/Pen-Pineapple-1052x591.png"
 ];
-var artists = [];
-var songLength = [];
-var links = [];
+var artists = [
+    "Luis Fonsi",
+    "PSY",
+    "Rebecca Black",
+    "Mishovy silenosti",
+    "Daimaou Kosaka"
+];
+var songLength = [
+    "4:41",
+    "4:12",
+    "3:48",
+    "1:16",
+    "2:43",
+    "",
+];
+var links = [
+    "https://www.youtube.com/watch?v=kJQP7kiw5Fk",
+    "https://www.youtube.com/watch?v=9bZkp7q19f0",
+    "https://www.youtube.com/watch?v=kfVsfOSbJY0",
+    "https://www.youtube.com/watch?v=vfc42Pb5RA8",
+    "https://www.youtube.com/watch?v=Ct6BUPvE2sM"
+];
 
 
 
@@ -33,7 +47,18 @@ function displaySongInfo(){
         $("#songs").append("<p>" + song + "</p>"); 
     });
     // BELOW Use forEach Loop to display the data from each of your array's in the correct div
-
+    images_urls.forEach(function(url) {
+        $("#images").append(`<img src="${url}">`); 
+    });
+    artists.forEach(function(artist) {
+        $("#artists").append("<p>" + artist + "</p>"); 
+    });
+    songLength.forEach(function(length) {
+        $("#lengths").append("<p>" + length + "</p>"); 
+    });
+    links.forEach(function(link,index) {
+        $("#links").append(`<a href="${link}">${songs[index]}</a>`); 
+    });
 }
 
 function emptySongInfo(){
